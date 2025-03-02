7897a40e86c9706dd8fbe2088835353ba443d191
calexil <admin@calexil.com>
2025-03-01 22:59:13 -0500

    0.0.1

--- BossBlindsBeat.lua
+++ BossBlindsBeat.lua
@@ -17,7 +17,7 @@ local lovely_dir_items = NFS.getInfo(SMODS.current_mod.path .. "lovely") and NFS
 local should_have_lovely = lovely_toml_info or (lovely_dir_items and #lovely_dir_items > 0)
 if should_have_lovely then
     -- if we have detected a `lovely.toml` file or a non-empty `lovely` directory (assumption that it contains lovely patches)
-    assert(SMODS.current_mod.lovely, "Failed to detect ossBlindsBeat lovely patches.\nMake sure your BossBlindsBeat folder is not nested (there should be a bunch of files in the BossBlindseat folder and not just another folder).\n\n\n")
+    assert(SMODS.current_mod.lovely, "Failed to detect BossBlindsBeat lovely patches.\nMake sure your BossBlindsBeat folder is not nested (there should be a bunch of files in the BossBlindsBeat folder and not just another folder).\n\n\n")
 end
 
 -- load all individual bosses
@@ -26,3 +26,26 @@ local bosses = NFS.getDirectoryItems(SMODS.current_mod.path .. subdir)
 for _, filename in pairs(bosses) do
     assert(SMODS.load_file(subdir .. "/" .. filename))()
 end
+
+local createOptionsRef = create_UIBox_options
+function create_UIBox_options()
+  contents = createOptionsRef()
+
+  if G.STATE ~= G.STATES.RUNINFO then
+    local bosses_tab = UIBox_tab({
+      minw = 5,
+      button = "bosses_tab",
+      label = {
+        "Bosses"
+      },
+      colour = HEX('E82C20')
+    })
+    table.insert(contents.nodes[1].nodes[1].nodes[1].nodes, #contents.nodes[1].nodes[1].nodes[1].nodes + 1, bosses_tab)
+  end
+  return contents
+end
+
+
+
+----------------------------------------------
+------------MOD CODE END----------------------
